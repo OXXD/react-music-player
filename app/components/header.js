e85fa802cfe0f1905c40b1e2931482de473d6a27
He Lin <oxxxxd@gmail.com>
2018-02-01 23:08:54 +0800

    Added: Progress component and jPlayer

--- app/components/header.js
+++ app/components/header.js
@@ -4,7 +4,7 @@ import './header.less'
 let Header = React.createClass({
     render() {
         return (
-            <div className="component-header row">
+            <div className="components-header row">
                 <img src="/static/images/logo.png" width="40" alt="" className="-col-auto" />
                 <h1 className="caption">React Music Player</h1>
             </div>    
