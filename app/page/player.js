d9b0312a9b02b22034bf965cb57257cd695c693e
He Lin <oxxxxd@gmail.com>
2018-02-05 02:59:06 +0800

    Caculate and formate a song's leftTime

--- app/page/player.js
+++ app/page/player.js
@@ -9,7 +9,8 @@ let Player = React.createClass({
         return {
             progress: 0,
             volume: 0,
-            isPlay: true
+            isPlay: true,
+            leftTime: ''
         }
     },
     playPrev() {
@@ -18,12 +19,30 @@ let Player = React.createClass({
     playNext() {
         PubSub.publish('PLAY_NEXT');
     },
+    play() {
+        if (this.state.isPlay) {
+            $('#player').jPlayer('pause');
+        } else {
+            $('#player').jPlayer('play');
+        }
+
+        this.setState({isPlay: !this.state.isPlay});
+    },
+    formatTime(time) {
+        time = Math.floor(time);
+        let minutes = Math.floor(time/ 60);
+        let seconds = Math.floor(time % 60);
+
+        seconds = seconds < 10 ? `0${seconds}` : seconds;
+        return `${minutes}:${seconds}`;
+    },
     componentDidMount() {
         $('#player').bind($.jPlayer.event.timeupdate, (e) => {
             duration = e.jPlayer.status.duration;
             this.setState({
                 volume: e.jPlayer.options.volume * 100,
-                progress: e.jPlayer.status.currentPercentAbsolute
+                progress: e.jPlayer.status.currentPercentAbsolute,
+                leftTime: this.formatTime(duration * (1 - e.jPlayer.status.currentPercentAbsolute / 100))
             })
         });
     },
@@ -36,15 +55,6 @@ let Player = React.createClass({
     changeVolumeHandler(progress) {
         $('#player').jPlayer('volume', progress);
     },
-    play() {
-        if (this.state.isPlay) {
-            $('#player').jPlayer('pause');
-        } else {
-            $('#player').jPlayer('play');
-        }
-
-        this.setState({isPlay: !this.state.isPlay});
-    },
     render() {
         return (
             <div className="player-page">
@@ -54,7 +64,7 @@ let Player = React.createClass({
                 		<h2 className="music-title">{this.props.currentMusicItem.title}</h2>
                 		<h3 className="music-artist mt10">{this.props.currentMusicItem.artist}</h3>
                 		<div className="row mt20">
-                			<div className="left-time -col-auto">-2:00</div>
+                			<div className="left-time -col-auto">-{this.state.leftTime}</div>
                 			<div className="volume-container">
                 				<i className="icon-volume rt" style={{top: 5, left: -5}}></i>
                 				<div className="volume-wrapper">
@@ -79,7 +89,7 @@ let Player = React.createClass({
 	                			<i className="icon next ml20" onClick={this.playNext}></i>
                 			</div>
                 			<div className="-col-auto">
-                				<i className={`icon repeat-`} ></i>
+                				<i className={`icon repeat-cycle`} ></i>
                 			</div>
                 		</div>
                 	</div>
